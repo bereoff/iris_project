1210979f60fc0b6870e602b49ac7c5b1d0aae031
Bruno Bereoff <bbereoff@gmail.com>
2020-06-25 15:40:34 +0100

    updating files and adding pictures for readme

--- model_building.py
+++ model_building.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 ## Data set reading
 
@@ -14,7 +15,7 @@ iris.head()
 from sklearn.model_selection import train_test_split
 
 X = iris.drop('species',axis=1)
-y = iris['species']
+y,levels = pd.factorize(iris['species'])
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
 
 # SVC
@@ -26,7 +27,7 @@ model = SVC()
 model.fit(X_train,y_train)
 
 # Model Prediction & Evaluation
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
 
 predictions = model.predict(X_test)
 
@@ -51,8 +52,8 @@ grid_predictions = grid.predict(X_test)
 
 #Results
 
-print(confusion_matrix(y_test,predictions))
-print(classification_report(y_test,predictions))
+print(confusion_matrix(y_test,grid_predictions))
+print(classification_report(y_test,grid_predictions))
 
 # Random Forest
 from sklearn.ensemble import RandomForestClassifier
@@ -70,4 +71,63 @@ rfc_pred = rfc.predict(X_test)
 
 print(confusion_matrix(y_test,rfc_pred))
 print(classification_report(y_test,rfc_pred))
+
+
+# Confusion Matrix plot
+
+## SVC
+
+cf_svc = confusion_matrix(y_test,predictions)
+print(cf_svc)
+
+sns.heatmap(cf_svc,annot=True,cmap='Blue')
+plt.show()
+
+# Confusion Matrix heatmap comparison among the models
+
+# SVC
+
+cf_svc = confusion_matrix(y_test,predictions)
+
+hm_svc = pd.DataFrame(cf_svc,
+                     index = ['setosa','versicolor','virginica'], 
+                     columns = ['setosa','versicolor','virginica'])
+
+plt.figure(figsize=(5.5,4))
+sns.heatmap(hm_svc, annot=True,cmap='YlGnBu')
+plt.title('SVC \nAccuracy:{0:.3f}'.format(accuracy_score(y_test, predictions)))
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+plt.show()
+
+# GridSearchCV
+
+cf_grid = confusion_matrix(y_test,grid_predictions)
+
+hm_grid = pd.DataFrame(cf_grid,
+                     index = ['setosa','versicolor','virginica'], 
+                     columns = ['setosa','versicolor','virginica'])
+
+plt.figure(figsize=(5.5,4))
+sns.heatmap(hm_grid, annot=True,cmap='YlGnBu')
+plt.title('GridSearchCV \nAccuracy:{0:.3f}'.format(accuracy_score(y_test, grid_predictions)))
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+plt.show()
+
+
+# Random Forest
+
+cf_rfc = confusion_matrix(y_test,rfc_pred)
+
+hm_rfc = pd.DataFrame(cf_rfc,
+                     index = ['setosa','versicolor','virginica'], 
+                     columns = ['setosa','versicolor','virginica'])
+
+plt.figure(figsize=(5.5,4))
+sns.heatmap(hm_rfc, annot=True,cmap='YlGnBu')
+plt.title('Random Forest \nAccuracy:{0:.3f}'.format(accuracy_score(y_test, rfc_pred)))
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+plt.show()
 
